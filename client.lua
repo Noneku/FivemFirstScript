ff6699aa2b29f76e77c278552e8cc030cb198998
Noneku <dev.nassim.pro@gmail.com>
2026-03-19 18:06:40 +0100

    feat(DetectionZoneState) : Display the distance between player and the target IN the zone

--- client.lua
+++ client.lua
@@ -10,16 +10,19 @@ CreateThread(function()
     while true do
         currentlyPos = math.floor(#(GetEntityCoords(playerPed) - startPos))
 
+        --Create a perimeter of 10 unities : Check if the player are inside or NOT 
         if currentlyPos < 10 and alreadyInzone == false then
-            TriggerEvent('chat:addMessage', {args = {'Zone : ', 'Entrée dans la zone!'}})
+            TriggerEvent('chat:addMessage', {args = {'Zone', 'Entrée dans la zone!'}})
             alreadyInzone = true
         elseif currentlyPos > 10 and alreadyInzone == true then
-            TriggerEvent('chat:addMessage', {args = {'Zone : ', 'Sortie de la zone'}})
-            print("You're leave the zone !")
+            TriggerEvent('chat:addMessage', {args = {'Zone', 'Sortie de la zone'}})
             alreadyInzone = false
-        end
+        end   
 
-        print(currentlyPos)
+            --Display the distance from the target and the player IN the zone
+            if alreadyInzone == true then
+                 TriggerEvent('chat:addMessage', {args = {'Cible', 'Vous êtes à ' .. currentlyPos .. " métres de votre cible"}})
+            end
     Wait(1000)
     end
 end)
