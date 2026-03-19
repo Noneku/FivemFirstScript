987a54b2c0274f7e4bc7243730fe6dffe2f350b0
Noneku <dev.nassim.pro@gmail.com>
2026-03-19 18:55:15 +0100

    feat(Command Car) : We can spawn inside the vehicle and remove de last vehicle spawned

--- client.lua
+++ client.lua
@@ -2,13 +2,14 @@ print("Ce script est prêt pour utilisation")
 
 --Create Zone Interactif
 local playerPed = PlayerPedId()
-local startPos = GetEntityCoords(playerPed)
+local playerPos = GetEntityCoords(playerPed)
+local vehicle = nil
 
 CreateThread(function()
     local currentlyPos = 0
     local alreadyInzone = false
     while true do
-        currentlyPos = math.floor(#(GetEntityCoords(playerPed) - startPos))
+        currentlyPos = math.floor(#(GetEntityCoords(playerPed) - playerPos))
 
         --Create a perimeter of 10 unities : Check if the player are inside or NOT 
         if currentlyPos < 10 and alreadyInzone == false then
@@ -26,3 +27,31 @@ CreateThread(function()
     Wait(1000)
     end
 end)
+
+-- Create command for spawn vehicle 
+RegisterCommand("spawncar", function(_, arg)
+    local modelCarAskByClient= arg[1]
+         --Check if the Model of the vehicle exists in CD image
+        if not IsModelInCdimage(modelCarAskByClient) or not IsModelAVehicle(modelCarAskByClient) then
+            TriggerEvent('chat:addMessage', {
+                args = {"Erreur", "Le modèle "..modelCarAskByClient.." n'existe pas !"}
+            })
+            return
+        end
+    RequestModel(modelCarAskByClient)
+    while not HasModelLoaded(modelCarAskByClient) do
+        Wait(100)
+    end
+
+    vehicle = CreateVehicle(modelCarAskByClient, playerPos.x, playerPos.y, playerPos.z, GetEntityHeading(playerPed), true, false)
+
+    SetPedIntoVehicle(playerPed, vehicle, -1)
+
+    SetModelAsNoLongerNeeded(modelCarAskByClient)
+end, false)
+
+-- Command for delete de last car loaded
+RegisterCommand("dv", function()
+    DeleteVehicle(vehicle)
+end, false)
+
